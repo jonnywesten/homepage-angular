c5dc3251a4e4d7abb56300a5670e57f81dbb42ee
Daniel <daniel.elport@itc-vogt.de>
2019-12-09 15:12:20 +0100

    -image loader -landing: animated stars background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,31 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
   constructor() {
   }
 
+  public loaded = false;
+  private imgs = new Array();
+
+
   ngOnInit(): void {
 
+    this.load(
+      'stars.jpg',
+      'twinkling.png'
+    );
+    this.loaded = true;
+  }
+
+  load(...args: any[]): void {
+    for (let i = 0; i < args.length; i++) {
+      this.imgs[i] = new Image();
+      this.imgs[i].src = 'assets/img/' + args[i];
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { PageContainerComponent } from './components/page-container/page-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    PageContainerComponent
   ],
   imports: [
     BrowserModule
