38eb6fc48ff3f14701ce27bc4e125d63c4048920
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-16 23:13:09 +0000

    copy_file

---
+++ react/src/utils/my-hammer.config.ts
@@ -0,0 +1,12 @@
+import {HammerGestureConfig} from '@angular/platform-browser';
+import * as hammer from 'hammerjs';
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = {
+    swipe: {direction: hammer.DIRECTION_HORIZONTAL},
+    pinch: {enable: false},
+    rotate: {enable: false}
+  } as any;
+}
