be2b5e58e7bc2f1368ffe11f42c834e5c53d5a62
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-16 23:13:19 +0000

    copy_file

---
+++ react/src/utils/header.component.ts
@@ -0,0 +1,40 @@
+<div class="header" [style.background-image]="'url(' + imageService.get('header-bg.jpg') + ')'">
+  <div class="content col-12">
+    <div class="profile-teaser media flex-column flex-md-row pt-md-5 mt-md-5 mb-5">
+
+      <img class="profile-image mr-5 d-none d-md-block" src="{{imageService.get('me.png')}}">
+      <div>
+
+        <h4 class="mb-2 text-inherit">{{text.headerSup}}</h4>
+        <h1 class="mt-0 mb-3 mr-2">
+          {{text.headerStart}}&nbsp;
+          <br class="d-sm-none"/>
+          <div class="animated-words">
+            <span>{{text.word1}}</span>
+            <span>{{text.word2}}</span>
+            <span>{{text.word3}}</span>
+          </div>
+          <br/>
+          {{text.headerEnd}}
+        </h1>
+
+        <img class="profile-image mb-4 d-md-none" src="{{imageService.get('me.png')}}">
+
+        <div class="border-top my-3 w-75"></div>
+
+        <p class="mb-md-3" [innerHTML]="text.text">
+
+        </p>
+
+        <div class="link my-3 d-inline-block" (click)="scrollService.scrollTo('#projects')">
+          <i class="mdi mdi-arrow-right-bold-outline"></i>
+          {{text.link1}}
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
